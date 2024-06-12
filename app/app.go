40bfe997f9738a19a987dbb9079609f5fd3520dc
Samuel Belo <belo.samuel@gmail.com>
2024-06-12 06:45:11 -0300

    search for server names added

--- app/app.go
+++ app/app.go
@@ -25,6 +25,17 @@ func Generate() *cli.App {
 			},
 			Action: searchIps,
 		},
+		{
+			Name:  "serverName",
+			Usage: "Searches for server name on the internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: searchServers,
+		},
 	}
 
 	return app
@@ -40,5 +51,19 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, error := net.LookupNS(host)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
